04bd36bce093906c705263995a8e79fade8d587c
GopherXZG <elijahbeyourself@gmail.com>
2024-03-09 15:34:08 +0800

    Used FAQ template

--- main.go
+++ main.go
@@ -37,8 +37,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ Page</h1><p>Is there a free version?</p><p>Yes</p>")
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func main() {
